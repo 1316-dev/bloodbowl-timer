a66a0050c72f157a5b2f90c60cf01000b5bfe8d7
1316-dev <mathieu.pineau@hotmail.fr>
2025-11-12 13:40:17 +0100

    calculate_the_duration_of_the_new_lap

--- timerApp.js
+++ timerApp.js
@@ -7,11 +7,12 @@ const TempsTourInitial = dureeTour*60;
 
 let tempsTour = TempsTourInitial;
 let tempsPartieJ1 = (dureeTour*NbTourParJoueur) * 60;
+let compteurTourJ1 = 0;
 
 const timerTourElement = document.getElementById("timerTour");
 
-let minutesTour 
-let secondesTour
+let minutesTour;
+let secondesTour;
 
 function demmarrerTour(){
   if(timerTourId){
@@ -43,25 +44,29 @@ function arreterTour() {
 
 function reinitialiserTour() {
   arreterTour();
-  tempsTour = TempsTourInitial;
+  tempsTour = ((minutesPartieJ1+heuresPartieJ1*60+secondesPartieJ1/60)*60)/(NbTourParJoueur-compteurTourJ1);
 }
 
 let $stop = document.getElementById("stop")
 $stop.addEventListener("click", function (e){
-console.log(minutesTour + ":" + secondesTour );
 arreter();
 arreterTour();
 reinitialiserTour();
+console.log(heuresPartieJ1 + minutesPartieJ1 +secondesPartieJ1)
+compteurTourJ1+=1
 });
 
 let $start = document.getElementById("start")
 $start.addEventListener("click", function (e){
-console.log(minutesTour + ":" + secondesTour );
 demmarrer();
 demmarrerTour();
 });
 
 const timerPartieElement = document.getElementById("timerPartie");
+
+let minutesPartieJ1;
+let secondesPartieJ1;
+let heuresPartieJ1; 
 
 function demmarrer(){
   if(timerId){
@@ -72,18 +77,18 @@ function demmarrer(){
 
 
   timerId = setInterval(() => {
-  let minutesPartieJ1 = parseInt(tempsPartieJ1 / 60, 10);
-  let secondesPartieJ1 = parseInt(tempsPartieJ1 % 60, 10);
-  let heuresPartieJ1 = Math.trunc(minutesPartieJ1 / 60);
+  minutesPartieJ1 = parseInt(tempsPartieJ1 / 60, 10);
+  secondesPartieJ1 = parseInt(tempsPartieJ1 % 60, 10);
+  heuresPartieJ1 = Math.trunc(minutesPartieJ1 / 60);
  
-  heuresPartieJ1 = heuresPartieJ1 < 10 ? "0" + heuresPartieJ1 + ":": heuresPartieJ1;
+  //heuresPartieJ1 = heuresPartieJ1 < 10 ? "0" + heuresPartieJ1 + ":": heuresPartieJ1;
   heuresPartieJ1 = heuresPartieJ1 < 1 ? " " : heuresPartieJ1;
   minutesPartieJ1 = minutesPartieJ1 > 60 ? (minutesPartieJ1-60) : minutesPartieJ1;
   minutesPartieJ1 = minutesPartieJ1 == 60 ? "0" : minutesPartieJ1;
   minutesPartieJ1 = minutesPartieJ1 < 10 ? "0" + minutesPartieJ1 : minutesPartieJ1;
   secondesPartieJ1 = secondesPartieJ1 < 10 ? "0" + secondesPartieJ1 : secondesPartieJ1;
 
-  timerPartieElement.innerText = `${heuresPartieJ1}${minutesPartieJ1}:${secondesPartieJ1}`;
+  timerPartieElement.innerText = `${heuresPartieJ1}:${minutesPartieJ1}:${secondesPartieJ1}`;
   tempsPartieJ1 = tempsPartieJ1 <= 0 ? 0 : tempsPartieJ1 - 1;
 }, 1000);
 }
