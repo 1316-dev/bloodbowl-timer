311f23bed48b17dd9baab56d635a14c4297d4aba
1316-dev <mathieu.pineau@hotmail.fr>
2026-02-21 11:43:41 +0100

    refactor  avec un objet joueurs

--- js/model.js
+++ js/model.js
@@ -6,6 +6,35 @@
 // - gestion des compteurs de tour et joueur actif
 // ===================================
 
+//================================
+// Création des joueurs
+//================================
+export const joueurs = {
+    1: {
+      tempsPartie : 0,
+        tempsRestantGlobal : 0,
+        timerPartie: 0,
+        timerTour: 0,
+        toursRestants: 0,
+        numeroTour: 0,
+        compteurTour: 0,
+        $btnStart : null
+        
+    
+    },
+    2: {
+       tempsPartie : 0,
+        tempsRestantGlobal : 0,
+        timerPartie: 0,
+        timerTour: 0,
+        toursRestants: 0,
+        numeroTour: 0,
+        compteurTour: 0,
+        $btnStart : null
+    }
+};
+
+
 //=================================
 // Paramètre lié au règle du jeu
 //==================================
@@ -32,10 +61,10 @@ export function convertMinutesToMinutesAndSeconds(minutesFloat) {
 
 export const NbTourParJoueur = 16;
 // Variables d'état (temps en secondes)
-export let tempsPartieJ1 = 0;
-export let tempsTourJ1 = 0;
-export let tempsPartieJ2 = 0;
-export let tempsTourJ2 = 0;
+// export let tempsPartieJ1 = 0;
+// export let tempsTourJ1 = 0;
+// export let tempsPartieJ2 = 0;
+// export let tempsTourJ2 = 0;
 
 export function calculerTempsInitiaux(
   heuresPartieChoisies,
@@ -52,10 +81,10 @@ export function calculerTempsInitiaux(
   const tempsTourSecondes = tempsTourMinutes * 60;
 
   // Mise à jour de l'état global (en secondes)
-  tempsPartieJ1 = tempsPartieTotalSecondes / 2;
-  tempsTourJ1 = tempsTourSecondes;
-  tempsPartieJ2 = tempsPartieTotalSecondes / 2;
-  tempsTourJ2 = tempsTourSecondes;
+  joueurs[1].tempsPartie = tempsPartieTotalSecondes / 2;
+  joueurs[1].tempsTour = tempsTourSecondes;
+  joueurs[2].tempsPartie = tempsPartieTotalSecondes / 2;
+  joueurs[2].tempsTour = tempsTourSecondes;
 
   // Retourne la valeur pour l'affichage
   return convertMinutesToMinutesAndSeconds(tempsTourMinutes);
@@ -79,11 +108,11 @@ export function calculerTempsEncours(
 
   // Mise à jour de l'état global (en secondes)
   if (joueur === 1) {
-    tempsPartieJ1 = tempsPartieTotalSecondes;
-    tempsTourJ1 = tempsTourSecondes;
+    joueurs[1].tempsPartie = tempsPartieTotalSecondes;
+    joueurs[1].tempsTour = tempsTourSecondes;
   } else if (joueur === 2) {
-    tempsPartieJ2 = tempsPartieTotalSecondes;
-    tempsTourJ2 = tempsTourSecondes;
+    joueurs[2].tempsPartie = tempsPartieTotalSecondes;
+    joueurs[2].tempsTour = tempsTourSecondes;
   }
 
   // Retourne la valeur pour l'affichage
@@ -94,16 +123,20 @@ export function calculerTempsEncours(
 // Mise à jour de l'état (fonctions de jeu)
 // =======================================
 
-export let compteurTourJ1 = 0;
-export let compteurTourJ2 = 0;
+// export let compteurTourJ1 = 0;
+// export let compteurTourJ2 = 0;
 
-export function setCompteurTourJ1(nouvelleValeur) {
-  compteurTourJ1 = nouvelleValeur;
+export function setCompteurTour(numeroJoueur, nouvelleValeur) {
+  joueurs[numeroJoueur].compteurTour = nouvelleValeur;
 }
 
-export function setCompteurTourJ2(nouvelleValeur) {
-  compteurTourJ2 = nouvelleValeur;
-}
+// export function setCompteurTourJ1(nouvelleValeur) {
+//   compteurTourJ1 = nouvelleValeur;
+// }
+
+// export function setCompteurTourJ2(nouvelleValeur) {
+//   compteurTourJ2 = nouvelleValeur;
+// }
 
 // Variables pour les IDs des intervalles, utilisé pour arrêter les timers
 export let timerIdJ1 = null;
@@ -117,44 +150,62 @@ export let joueurActif = null;
 // L'intervalle de temps (1 seconde)
 export function decrementerTemps() {
   if (joueurActif === 1) {
-    tempsPartieJ1 = tempsPartieJ1 <= 0 ? 0 : tempsPartieJ1 - 1;
-    tempsTourJ1 = tempsTourJ1 <= 0 ? 0 : tempsTourJ1 - 1;
+    joueurs[1].tempsPartie = joueurs[1].tempsPartie <= 0 ? 0 : joueurs[1].tempsPartie - 1;
+    joueurs[1].tempsTour = joueurs[1].tempsTour <= 0 ? 0 : joueurs[1].tempsTour - 1;
   } else if (joueurActif === 2) {
-    tempsPartieJ2 = tempsPartieJ2 <= 0 ? 0 : tempsPartieJ2 - 1;
-    tempsTourJ2 = tempsTourJ2 <= 0 ? 0 : tempsTourJ2 - 1;
+    joueurs[2].tempsPartie = joueurs[2].tempsPartie <= 0 ? 0 : joueurs[2].tempsPartie - 1;
+    joueurs[2].tempsTour = joueurs[2].tempsTour <= 0 ? 0 : joueurs[2].tempsTour - 1;
   }
 }
 
 
+export function reinitialiserTour(numeroJoueur) {
+    const joueur = joueurs[numeroJoueur];
 
-export function reinitialiserTour(joueur) {
   // Calcul de la durée du prochain tour : Temps global restants diviser par le nombre de tour restant
-  if (joueur === 1) {
-    let tempsRestantGlobalJ2 = tempsPartieJ2;
-    let toursRestantsJ2 = NbTourParJoueur - compteurTourJ2;
-    if (toursRestantsJ2 > 0) {
+    joueur.tempsRestantGlobal = joueur.tempsPartie;
+    joueur.toursRestants = NbTourParJoueur - joueur.compteurTour;
+    
+    if (joueur.toursRestants > 0) {
       // IMPORTANT : on réinitialise le tour du prochain joueur
-      tempsTourJ2 = tempsRestantGlobalJ2 / toursRestantsJ2;
+      joueur.tempsTour = joueur.tempsRestantGlobal / joueur.toursRestants;
     }
-  } else if (joueur === 2) {
-    let tempsRestantGlobalJ1 = tempsPartieJ1;
-    let toursRestantsJ1 = NbTourParJoueur - compteurTourJ1;
-    if (toursRestantsJ1 > 0) {
-      // IMPORTANT : on réinitialise le tour du prochain joueur
-      tempsTourJ1 = tempsRestantGlobalJ1 / toursRestantsJ1;
-      console.log(tempsTourJ1, toursRestantsJ1);
-    }
-  }
+  } 
+
+  export function passerAuJoueur(numeroJoueur) {
+    const joueur = joueurs[numeroJoueur];
+    joueur.compteurTour += 1;
+  joueurActif = numeroJoueur;
 }
 
-export function passerAuJoueur(joueurSuivant) {
-  if (joueurSuivant === 1) {
-    compteurTourJ1 += 1;
-  } else if (joueurSuivant === 2) {
-    compteurTourJ2 += 1;
-  }
-  joueurActif = joueurSuivant;
-}
+// export function reinitialiserTour(joueur) {
+//   // Calcul de la durée du prochain tour : Temps global restants diviser par le nombre de tour restant
+//   if (joueur === 1) {
+//     let tempsRestantGlobalJ2 = tempsPartieJ2;
+//     let toursRestantsJ2 = NbTourParJoueur - compteurTourJ2;
+//     if (toursRestantsJ2 > 0) {
+//       // IMPORTANT : on réinitialise le tour du prochain joueur
+//       tempsTourJ2 = tempsRestantGlobalJ2 / toursRestantsJ2;
+//     }
+//   } else if (joueur === 2) {
+//     let tempsRestantGlobalJ1 = tempsPartieJ1;
+//     let toursRestantsJ1 = NbTourParJoueur - compteurTourJ1;
+//     if (toursRestantsJ1 > 0) {
+//       // IMPORTANT : on réinitialise le tour du prochain joueur
+//       tempsTourJ1 = tempsRestantGlobalJ1 / toursRestantsJ1;
+//       console.log(tempsTourJ1, toursRestantsJ1);
+//     }
+//   }
+// }
+
+// export function passerAuJoueur(joueurSuivant) {
+//   if (joueurSuivant === 1) {
+//     compteurTourJ1 += 1;
+//   } else if (joueurSuivant === 2) {
+//     compteurTourJ2 += 1;
+//   }
+//   joueurActif = joueurSuivant;
+// }
 
 // =======================================
 // Mise à jour de l'état du compteur Pause
