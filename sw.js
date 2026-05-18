2c7c7a5b4b06283d267c40ab9f5af88f8072f3d9
1316-dev <mathieu.pineau@hotmail.fr>
2026-05-18 11:55:01 +0200

    correction service worker

--- sw.js
+++ sw.js
@@ -24,13 +24,22 @@ const FILES = [
 
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open(CACHE).then(c => c.addAll(FILES))
+        caches.open(CACHE)
+            .then(c => c.addAll(FILES))
+            .then(() => self.skipWaiting()) 
     );
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(clients.claim());
+    e.waitUntil(
+        // Nettoie les anciens caches
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => clients.claim())
+    );
 });
 
 self.addEventListener('fetch', e => {
