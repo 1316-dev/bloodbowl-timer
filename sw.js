65045f1dbbd2899da1bf7b19122db86cb2f0273a
1316-dev <mathieu.pineau@hotmail.fr>
2026-05-18 11:36:32 +0200

    service worker modifie pour deploiement auto

--- sw.js
+++ sw.js
@@ -23,19 +23,18 @@ const FILES = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(FILES))
-      .then(() => console.log('SW installé avec succès'))
-      .catch(err => {
-        console.error('SW installation échouée :', err); 
-        throw err;
-      })
-  );
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(FILES))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
 });
